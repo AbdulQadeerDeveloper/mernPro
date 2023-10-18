feb7c6314a9e3ded857e41521ed41405c5ee87d7
Abdul Qadeer <qadeerdeveloper312@gmail.com>
2023-10-18 13:56:47 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Register from './components/User/Register';
+import Login from './components/User/Login';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ChakraProvider } from '@chakra-ui/react';
+import Home from './components/Home/Home';
+import MyHome from "./components/Home/MyHome";
+import { PrivateRoutes } from './routes/PrivateRoutes';
+import { OpenRoutes } from './routes/OpenRoutes';
+import Video from './components/Video/Video';
+import Profile from './components/User/Profile';
+import Wallet from './components/Wallet/Wallet';
+import WithDraw from "./components/Wallet/WithDraw";
+import Team from './components/Team/Team';
+import Invite from './components/Invite/Invite';
+import AcceptInvite from './components/Invite/AcceptInvite';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/register' element={<OpenRoutes><Register /></OpenRoutes>} />
+          <Route path='/login' element={<OpenRoutes><Login /></OpenRoutes>} />
+          <Route path='/' element={<PrivateRoutes><Home /></PrivateRoutes>} />
+          <Route path='/home' element={<PrivateRoutes><MyHome /></PrivateRoutes>} />
+          <Route path='/video' element={<PrivateRoutes><Video /></PrivateRoutes>} />
+          <Route path='/profile' element={<PrivateRoutes><Profile /></PrivateRoutes>} />
+          <Route path='/wallet' element={<PrivateRoutes><Wallet /></PrivateRoutes>} />
+          <Route path='/withdraw' element={<PrivateRoutes><WithDraw /></PrivateRoutes>} />
+          <Route path='/team' element={<PrivateRoutes><Team /></PrivateRoutes>} />
+          <Route path='/invite' element={<PrivateRoutes><Invite /></PrivateRoutes>} />
+          <Route path='/accept-invite/:id' element={<PrivateRoutes><AcceptInvite /></PrivateRoutes>} />
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
   );
 }
 
